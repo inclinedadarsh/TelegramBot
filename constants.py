59c429e5c209089690efa8a1f0e471d885d5941e
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2021-05-13 17:46:00 +0530

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,1 @@
+API_KEY = 'your bot API Token'
---
+++ responses.py
@@ -0,0 +1,20 @@
+def sample_responses(input_text):
+    user_message = str(input_text).lower()
+
+    if user_message in "Hey":
+        return "Heyy Buddy, Nice To See You. Start a quick game of Truth or Dare :)"
+
+    if user_message in "Hi":
+        return "Heyy Buddy, Nice To See You. Start a quick game of Truth or Dare :)"
+
+    if user_message in "Heyy":
+        return "Heyy Buddy, Nice To See You. Start a quick game of Truth or Dare :)"
+
+    if user_message in "Hii":
+        return "Heyy Buddy, Nice To See You. Start a quick game of Truth or Dare :)"
+
+    if user_message in "Hello":
+        return "Heyy Buddy, Nice To See You. Start a quick game of Truth or Dare :)"
+
+    return "I Don't Understand, obviously because I'm a Bot, @inclinedadarsh didn't teach me much. Still you can type " \
+           "/truthclean and other commands for me to begin. "
